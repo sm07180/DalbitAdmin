36ea639d60973c04671bc88205758abfd8004572
이재은 <coreano81@inforex.co.kr>
2020-01-16 17:08:48 +0900

    오플리케이션 이름변경

--- src/main/java/com/dalbit/AdminApplication.java
+++ src/main/java/com/dalbit/AdminApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class AdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DemoApplication.class);
+        return builder.sources(AdminApplication.class);
     }
 }
