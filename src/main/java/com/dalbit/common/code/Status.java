1a80caaea7c72db34ed5b57e6975d7bf62e82942
고병권 <zox570215@gamil.com>
2020-02-04 18:03:05 +0900

    회원정보 상세보기 데이터 송수신 진행 중

--- src/main/java/com/dalbit/common/code/Status.java
+++ src/main/java/com/dalbit/common/code/Status.java
@@ -18,6 +18,11 @@ public enum Status {
     엑셀다운로드성공("1", "excel.download.success", "엑셀 다운로드 성공 시"),
     엑셀다운로드실패("0", "excel.download.fail", "엑셀 다운로드 실패 시"),
 
+    //회원정보보기 (프로필, 마이페이지 통합)
+    회원정보보기_성공("0", "member.info.view.success", "회원 팬 등록 성공 시"),
+    회원정보보기_회원아님("-1", "member.no", "회원이 아닐 시"),
+    회원정보보기_대상아님("-2", "member.this.no", "회원 팬 등록 실패 시 "),
+    회원정보보기_실패("C006", "member.info.view.fail", "회원정보보기 실패 시"),
     ;
 
     final private String SUCCESS_RESULT = "success";
