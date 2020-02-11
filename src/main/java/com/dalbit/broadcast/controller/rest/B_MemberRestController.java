2b521b28df2fab68b96b6b68fa3dec8a10648e58
고병권 <zox570215@gamil.com>
2020-02-11 15:56:44 +0900

    방송관리 개발 진행중 - 청취자 세부내역 개발

--- src/main/java/com/dalbit/broadcast/controller/rest/B_MemberRestController.java
+++ src/main/java/com/dalbit/broadcast/controller/rest/B_MemberRestController.java
@@ -3,6 +3,7 @@ package com.dalbit.broadcast.controller.rest;
 
 import com.dalbit.broadcast.service.B_BroadcastService;
 import com.dalbit.broadcast.service.B_MemberService;
+import com.dalbit.broadcast.vo.BroadcastHistroyVo;
 import com.dalbit.common.code.Status;
 import com.dalbit.common.vo.JsonOutputVo;
 import com.dalbit.util.GsonUtil;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -28,31 +31,21 @@ public class B_MemberRestController {
     GsonUtil gsonUtil;
 
     /**
-     * 회원리스트
-     * @param model
+     * 방송관리 청취자 정보
+     * //@param model
      * @return
      */
 
-    @PostMapping("list")
-    public String list(Model model){
-        ArrayList<HashMap> list = new ArrayList<HashMap>();
-        HashMap map = new HashMap();
-        for(int i=0; i++ < 20;) {
+    @PostMapping("broadcastHistory_detail")
+    public String broadcastHistory_detail(HttpServletRequest request){
+        log.info("@@@ 1");
+        BroadcastHistroyVo apiData = new BroadcastHistroyVo();
+        apiData.setRoomNo((String) request.getParameter("roomNo"));
+        apiData.setTmp((String) request.getParameter("tmp"));
+        log.info("@@@ " + (String) request.getParameter("roomNo") + (String) request.getParameter("tmp"));
 
-            map.put("NO", i);
-            map.put("MemNo", "11577950603958");
-            map.put("UserID", "3333");
-            map.put("NickName", "cccc");
-            map.put("Name", "양달님");
-            map.put("PhoneNum", "010-9941-0000");
-            map.put("JoinPlatform", "달빛");
-            map.put("Login_out", "Login");
-            map.put("Live", "생방중♠");
-            list.add(map);
-        }
-
-
-        return gsonUtil.toJson(new JsonOutputVo(Status.조회, list));
+        List<BroadcastHistroyVo> list = bMemberService.getBroadcastHistory_detail(apiData);
+        return gsonUtil.toJson(new JsonOutputVo(Status.회원정보보기_성공, list));
     }
 
 }
