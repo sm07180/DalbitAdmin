818e28d2603b841f34286f3dacd5fad1ea730653
고병권 <zox570215@gamil.com>
2020-02-26 10:49:16 +0900

    회원정보 - 텝 추가 테이블 테스트

--- src/main/webapp/js/dataTablesSource/memberDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/memberDataTableSource.js
@@ -13,9 +13,10 @@ var MemberDataTableSource = {
              {'title': '접속상태', 'data': 'Login_out', 'defaultContent': '로그아웃', 'width':'100px'},
              {'title': '생방상태', 'data': 'Live', 'defaultContent': '퇴근', 'width':'100px'},
         ]
+        , 'comments': '회원 아이디를 클릭하시면 상세정보를 확인할 수 있습니다.'
     },
 
-    'broadHistory': {
+    'broadDetail': {
         'url': '/rest/member/broadcast/list'
         , 'columns': [
             {'title': 'roomNo', 'data': 'roomNo', 'visible' : false},
@@ -23,105 +24,84 @@ var MemberDataTableSource = {
             {'title': '방송제목', 'data': 'title', 'width':'250px', 'render': function (data, type, row, meta) {
                     return '<a href="javascript://" onclick="javascript:Broad('+meta.row+');">' + data + '</a>'
                 }},
-            {'title': '방송시작시간', 'data': 'startDate', 'width':'100px'},
-            {'title': '방송종료시간', 'data': 'endDate', 'width':'100px'},
+            {'title': '방송시작시간', 'data': 'startDate', 'width':'120px'},
+            {'title': '방송종료시간', 'data': 'endDate', 'width':'120px'},
             {'title': '방송진행시간', 'data': 'airtime', 'width':'100px'},
-            {'title': '종료시평균청취자수', 'data': 'listener', 'width':'100px'},
-            {'title': '받은좋아요수', 'data': 'good', 'width':'80px'},
-            {'title': '받은아이템수', 'data': 'gold', 'width':'80px'},
+            {'title': '청취자', 'data': 'listener', 'width':'80px'},
+            {'title': '누적청취자', 'data': 'listenHistory', 'width':'80px'},
+            {'title': '받은좋아요', 'data': 'good', 'width':'80px'},
         ]
-        , 'title': '방송기록'
         , 'comments': '회원이 방송을 진행하고, 청취한 기록을 확인할 수 있습니다.'
     },
 
-    'listenHistory': {
+    'listenDetail': {
         'url': '/rest/member/listen/list'
         , 'columns': [
             {'title': 'roomNo', 'data': 'roomNo' , 'visible' : false},
-            {'title': '청취방주제', 'data': 'subjectType'},
-            {'title': '청취방송제목', 'data': 'title', 'render': function (data, type, row, meta) {
+            {'title': 'DJID', 'data': 'memId', 'width':'100px'},
+            {'title': 'DJ닉네임', 'data': 'memNick', 'width':'100px'},
+            {'title': '청취방주제', 'data': 'subjectType', 'width':'100px'},
+            {'title': '청취방송제목', 'data': 'title', 'width':'250px', 'render': function (data, type, row, meta) {
                     return '<a href="javascript://" onclick="javascript:Listen('+meta.row+')">' + data + '</a>'
                 }},
-            {'title': '청취시작시간', 'data': 'startDate'},
-            {'title': '청취종료시간', 'data': 'endDate'},
-            {'title': '청취진행시간', 'data': 'listenTime'},
-            {'title': 'DJID', 'data': 'memId'},
-            {'title': 'DJ닉네임', 'data': 'memNick'},
-            {'title': '보낸좋아요수', 'data': 'like'},
-            {'title': '보낸아이템수', 'data': 'ruby'}
+            {'title': '청취시작시간', 'data': 'startDate', 'width':'120px'},
+            {'title': '청취종료시간', 'data': 'endDate', 'width':'120px'},
+            {'title': '청취진행시간', 'data': 'listenTime', 'width':'100px'},
+            {'title': '좋아요', 'data': 'like', 'width':'80px'},
+            {'title': '보낸아이템수', 'data': 'item', 'width':'80px'},
         ]
-        , 'title': '청취기록'
-        , 'comments': '회원이 방송을 청취한 과거기록을 확인할 수 있습니다.'
+        , 'comments': '회원이 방송을 청취한 기록을 확인할 수 있습니다.'
     },
 
-    'payHistory': {
+    'payDetail': {
         'url': '/rest/member/pay/list'
         , 'columns': [
             {'title': '구분', 'data': '' , 'defaultContent': ''},
-            {'title': '아이템사용유무', '': '', 'defaultContent': ''},
+            {'title': '환불가능여부', '': '', 'defaultContent': ''},
             {'title': '아이템명', 'data': '', 'defaultContent': ''},
-            {'title': '환불건수', 'data': '', 'defaultContent': ''},
-            {'title': '환불수단', 'data': ' ', 'defaultContent': ''},
-            {'title': '요청금액', 'data': '', 'defaultContent': ''},
-            {'title': '환불일시', 'data': '', 'defaultContent': ''},
-            {'title': '처리일시', 'data': '', 'defaultContent': ''},
-            {'title': '처리상태', 'data': '', 'defaultContent': ''},
-            {'title': '처리자ID', 'data': '', 'defaultContent': ''}
+            {'title': '결제/환불 건 수', 'data': '', 'defaultContent': ''},
+            {'title': '결제/환불 수단', 'data': ' ', 'defaultContent': ''},
+            {'title': '요청 금액', 'data': '', 'defaultContent': ''},
+            {'title': '결제/환불 일시', 'data': '', 'defaultContent': ''},
+            {'title': '처리 일시', 'data': '', 'defaultContent': ''},
+            {'title': '처리 상태', 'data': '', 'defaultContent': ''},
+            {'title': '처리자ID', 'data': '', 'defaultContent': ''},
         ]
-        , 'title': '결제/환불정보'
-        , 'comments': '회원의 결제정보를확인하고 결제 취소처리를 할 수 있습니다.'
+        , 'comments': '회원의 결제/환불 정보를 확인하고, 결제 건에 한해 구분>결제를 클릭 시 취소처리를 할 수 있습니다.'
     },
 
-    'giftHistory': {
+    'exchangeDetail': {
+        'url': '/rest/member/exchange/list'
+        , 'columns': [
+            {'title': 'UserID', 'data': '', 'defaultContent': ''},
+            {'title': '닉네임', 'data': '', 'defaultContent': ''},
+            {'title': '환전요청금액', 'data': '', 'defaultContent': ''},
+            {'title': '처리일시', 'data': '', 'defaultContent': ''},
+            {'title': '신청 골드 금액', 'data': '', 'defaultContent': ''},
+            {'title': '누적환전건수', 'data': '', 'defaultContent': ''},
+            {'title': '누적완료건수', 'data': '', 'defaultContent': ''},
+            {'title': '처리상태', 'data': '', 'defaultContent': ''},
+            {'title': '상세보기', 'data': '', 'defaultContent': ''},
+            {'title': '처리자ID', 'data': '', 'defaultContent': ''},
+        ]
+        , 'comments': '회원의 과거에서 현재까지의 환전 내역을 모두 확인하고, 증빙서류를 통한 본인확인 여부를 확인 할 수 있습니다.'
+    },
+
+    'giftDetail': {
         'url': '/rest/member/gift/list'
         , 'columns': [
             {'title': '구분', 'data': '', 'defaultContent': ''},
             {'title': '아이템명', 'data': '', 'defaultContent': ''},
             {'title': '보낸/받은/교환건수', '': 'giftCnt', 'defaultContent': ''},
-            {'title': '교환/선물금액', 'data': '', 'defaultContent': ''},
-            {'title': '교환/선물일시', 'data': '', 'defaultContent': ''},
+            {'title': '선물/교환 금액', 'data': '', 'defaultContent': ''},
+            {'title': '선물/교환 일시', 'data': '', 'defaultContent': ''},
             {'title': 'UserID', 'data': '', 'defaultContent': ''},
             {'title': 'User닉네임', 'data': '', 'defaultContent': ''},
-            {'title': '처리자ID', 'data': '', 'defaultContent': ''},
         ]
-        , 'title': '교환/선물정보'
-        , 'comments': '회원이 달에서 별로 교환한 정보를 확인하고, 보내고 받은 선물 내역을 확인할 수 있습니다.이벤트 당첨선물로 제공되어 처리된 사안일 경우 처리직원ID로 표기됩니다.'
+        , 'comments': '회원이 보내고 받은 선물 내역과 달에서 별로 교환한 정보를 확인 할 수 있습니다. 이벤트에 당첨되어 받은 선물은 "받은선물-이벤트"로 부분되어 이벤트 당첨 - 당첨선물'
     },
 
-    'exchangeHistory': {
-        'url': '/rest/member/exchange/list'
-        , 'columns': [
-            {'title': '보유골드수', 'data': '', 'defaultContent': ''},
-            {'title': '환전요청건수', 'data': '', 'defaultContent': ''},
-            {'title': '환전요청금액', 'data': '', 'defaultContent': ''},
-            {'title': '환전신청일', 'data': '', 'defaultContent': ''},
-            {'title': '환전처리일', 'data': '', 'defaultContent': ''},
-            {'title': '증빙서류확인', 'data': '', 'defaultContent': ''},
-            {'title': '본인확인', 'data': '', 'defaultContent': ''},
-            {'title': '환전상태', 'data': '', 'defaultContent': ''},
-            {'title': '처리자ID', 'data': '', 'defaultContent': ''},
-        ]
-        , 'title': '개인환불정보'
-        , 'comments': '회원의 과거에서 현재까지의 환전 내역을 모두 확인하고, 증빙서류를 확인한 후 즉시 처리할 수 있습니다.'
-    },
-
-    'platformHistory': {
-        'url': '/rest/member/platform/list'
-        , 'columns': [
-            {'title': '접속일시', 'data': '', 'defaultContent': ''},
-            {'title': 'OS환경', 'data': '', 'defaultContent': ''},
-            {'title': 'AppVer', 'data': '', 'defaultContent': ''},
-            {'title': 'Browser', 'data': '', 'defaultContent': ''},
-            {'title': 'Device', 'data': '', 'defaultContent': ''},
-            {'title': 'MobileDeviceID', 'data': '', 'defaultContent': ''},
-            {'title': 'MobileADID', 'data': '', 'defaultContent': ''},
-            {'title': 'IP주소', 'data': '', 'defaultContent': ''},
-        ]
-        , 'title': '회원 접속 기록'
-        , 'comments': '회원의 가입일로부터의 접속 기록을 확인할 수 있습니다.'
-    },
-
-    'myStarHistory': {
+    'mystarDetail': {
         'url': '/rest/member/myStar/list'
         , 'columns': [
             {'title': 'memNo', 'data': 'memNo', 'visible' : false},
@@ -129,93 +109,69 @@ var MemberDataTableSource = {
                     return '<a href="javascript://" onclick="javascript:MyStar('+meta.row+');">' + data + '</a>'
                 }},
             {'title': 'MyStar닉네임', 'data': 'memNick'},
-            {'title': '보낸건수', 'data': 'tmp1'},
-            {'title': '보낸개수', 'data': 'tmp2'},
-            {'title': '등록일시', 'data': 'regDate'},
-            {'title': '해제일시', 'data': 'tmp3'}
+            {'title': '아이템보낸건수', 'data': 'tmp1'},
+            {'title': '아이템보낸개수', 'data': 'tmp2'},
+            {'title': 'FanID', 'data': 'tmp2'},
+            {'title': 'Fan닉네임', 'data': 'tmp2'},
+            {'title': '선물 받은 건 수', 'data': 'regDate'},
+            {'title': '선물 받은 금액', 'data': 'tmp3'}
         ]
-        , 'title': 'MyStar 정보'
-        , 'comments': 'MyStar 회원 리스트를 확인하고 등록/삭제 할 수 있습니다.'
+        , 'comments': 'MyStar : 내가 Fan으로 등록하거나, 방송 중 선물을 보낸 회원입니다. Fan: 타 회원이 Fan으로 등록하면 해당회원이 MyStar 가 됩니다.'
     },
 
-    'myFanHistory': {
-        'url': '/rest/member/fan/list'
-        , 'columns': [
-            {'title': 'memNo', 'data': 'memNo', 'visible' : false},
-            {'title': 'FanID', 'data': 'memId', 'render': function (data, type, row, meta) {
-                    return '<a href="javascript://" onclick="javascript:Fan('+meta.row+');">' + data + '</a>'
-                }},
-            {'title': 'Fan닉네임', 'data': 'memNick'},
-            {'title': '보낸건수', 'data': 'tmp1'},
-            {'title': '보낸개수', 'data': 'tmp2'},
-            {'title': '등록일시', 'data': 'regDate'},
-            {'title': '해제일시', 'data': 'tmp3'}
-        ]
-        , 'title': 'Fan 정보'
-        , 'comments': 'Fan으로 등록한 회원들의 리스트를 확인하고 등록/삭제 할 수 있습니다.'
-    },
-
-    'noticeHistory': {
+    'noticeDetail': {
         'url': '/rest/member/notice/list'
         , 'columns': [
-            {'title': 'roomNo', 'data': 'roomNo', 'visible' : false},
-            {'title': '구분', 'data': 'type'},
+            {'title': '프로필이미지', 'data': 'profile'},
+            {'title': '닉네임', 'data': 'type'},
             {'title': '방송제목', 'data': 'title', 'render': function (data, type, row, meta) {
                     return '<a href="javascript://" onclick="javascript:Notice('+meta.row+');">' + data + '</a>'
                 }},
             {'title': '공지내용', 'data': 'notice'},
             {'title': '등록일시', 'data': 'lastUpdDate'}
         ]
-        , 'title': '개별/방송 중 공지'
         , 'comments': '각 회원의 방송중 공지와 팬보드 내 연동된 공지 및 팬보드에서의 개인공지를 확인하고, 관리할 수 있습니다.'
     },
 
-    'reportHistory': {
+    'fanbordDetail': {
+        'url': '/rest/member/fanbord/list'
+        , 'columns': [
+            {'title': '프로필이미지', 'data': '', 'defaultContent': ''},
+            {'title': '닉네임', 'data': '', 'defaultContent': ''},
+            {'title': '아이디', 'data': '', 'defaultContent': ''},
+            {'title': '등록일시', 'data': '', 'defaultContent': ''},
+            {'title': '작성내용', 'data': '', 'defaultContent': ''},
+            {'title': '총 댓글 수', 'data': '', 'defaultContent': ''},
+        ]
+        , 'comments': '회원의 팬보드 내 작성 된 팬보드에서의 작성 글 및 댓글정보를 확인 할 수 있습니다.'
+    },
+
+    'banDetail': {
+        'url': '/rest/member/ban/list'
+        , 'columns': [
+            {'title': '금지어', 'data': '', 'defaultContent': ''},
+        ]
+        , 'comments': '회원이 직접 100개까지 관리를 할 수 있고, 관리자에 의해 등록/수정/삭제도 할 수 없습니다.'
+    },
+
+    'receiptDetail': {
         'url': '/rest/member/report/list'
         , 'columns': [
             {'title': 'idx', 'data': 'idx', 'visible' : false},
-            {'title': '플랫폼구분', 'data': 'platform'},
             {'title': '문의구분', 'data': 'type'},
-            {'title': '신고UserId', 'data': 'reportId'},
-            {'title': '신고대상UserID', 'data': 'reportMemId'},
-            {'title': '신고내용', 'data': 'etc', 'render': function (data, type, row, meta) {
-
-                    return '<a href="javascript://" onclick="javascript:Report('+meta.row+');">' + data + '</a>'
-                }},
+            {'title': '신고구분', 'data': 'type'},
+            {'title': '신고Id', 'data': 'reportId'},
+            {'title': '신고자닉네임', 'data': 'reportMemId'},
+            {'title': '신고대상Id', 'data': 'reportId'},
+            {'title': '신고대상닉네임', 'data': 'reportMemId'},
             {'title': '접수일시', 'data': 'lastUpdDate'},
             {'title': '처리일시', 'data': 'deployDate'},
             {'title': '처리상태', 'data': 'status'},
-            {'title': '처리자', 'data': 'deployUser'}
+            {'title': '처리자', 'data': 'deployUser'},
+            {'title': '처리상태', 'data': 'etc', 'render': function (data, type, row, meta) {
+                    return '<a href="javascript://" onclick="javascript:Report('+meta.row+');">' + data + '</a>'
+                }},
         ]
-        , 'title': '신고 처리 정보'
         , 'comments': '회원이 신고하고, 신고당한 정보를 한눈에 확인할 수 있습니다.'
     },
-
-    'responseHistory': {
-        'url': '/rest/member/response/list'
-        , 'columns': [
-            {'title': '플랫폼구분', 'data': '', 'defaultContent': ''},
-            {'title': '문의구분', 'data': '', 'defaultContent': ''},
-            {'title': '문의UserID', 'data': '', 'defaultContent': ''},
-            {'title': '문의대상UserID', 'data': '', 'defaultContent': ''},
-            {'title': '문의 제목', 'data': '', 'defaultContent': ''},
-            {'title': '접수일시/처리일시', 'data': '', 'defaultContent': ''},
-            {'title': '첨부파일', 'data': '', 'defaultContent': ''},
-            {'title': '처리상태', 'data': '', 'defaultContent': ''},
-            {'title': '처리자', 'data': '', 'defaultContent': ''},
-        ]
-        , 'title': '1:1 문의 정보'
-        , 'comments': '1:1문의 또는 전화로 문의한 내용의 세부 정보를 확인할 수 있도록 연동 되어있습니다.'
-    },
-
-    'editDateHistory': {
-        'url': '/rest/member/editdate/list'
-        , 'columns': [
-            {'title': '정보수정일시', 'data': '', 'defaultContent': ''},
-            {'title': '수정세부내용', 'data': '', 'defaultContent': ''},
-            {'title': '수정처리자', 'data': '', 'defaultContent': ''},
-        ]
-        , 'title': '정보수정 내역'
-        , 'comments': '회원 또는 운영자에 의해 정보가 수정된 일시를 확인할 수 있습니다.'
-    }
 }
