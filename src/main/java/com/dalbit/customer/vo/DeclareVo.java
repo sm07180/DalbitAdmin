0da442ed7c9fc0031219ec13f0c56fc2d590572b
강다인 <daink@inforex.co.kr>
2020-03-02 17:36:58 +0900

    신고센터

--- src/main/java/com/dalbit/customer/vo/DeclareVo.java
+++ src/main/java/com/dalbit/customer/vo/DeclareVo.java
@@ -11,15 +11,27 @@ public class DeclareVo extends PagingVo {
     /* input */
     private String search;
     private String searchType;
+    private String stopType;
 
     /* output */
-    private int type;
+/*    private int type;
     private String reason;
     private String memNo;
-    private String reportMemNo;
-    private String updDate;
+    private String reportedMemNo;
+    private String lastUpdDate;
     private String stateDate;
     private String state;
     private String stateUser;
+    private String memId;
+    private String memNick;
+    private String memPhone;*/
+
+    /* 임시 output*/
+    private String memNo;
+    private String memBirth;
+    private String memId;
+    private String memJoin;
+    private String memState;
+    private String memPhone;
 
 }
