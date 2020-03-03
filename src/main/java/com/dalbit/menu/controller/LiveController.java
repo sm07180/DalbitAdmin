0e04553a60d15f79c5cd2d16815988d14b95150c
이재호 <leejaeho114@gmail.com>
2020-03-03 17:58:24 +0900

    수정~~

---
+++ src/main/java/com/dalbit/menu/controller/LiveController.java
@@ -0,0 +1,16 @@
+package com.dalbit.menu.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("menu/live")
+public class LiveController {
+
+    @RequestMapping("list")
+    public String list(){
+        return "menu/live/list";
+    }
+}
