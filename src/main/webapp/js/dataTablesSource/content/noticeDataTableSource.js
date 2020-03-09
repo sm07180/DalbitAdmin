dff4ebb1254bbb9a982ee1d4ca079e6afd854cb4
강다인 <daink@inforex.co.kr>
2020-03-09 15:23:58 +0900

    수정

--- src/main/webapp/js/dataTablesSource/content/noticeDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/content/noticeDataTableSource.js
@@ -3,15 +3,20 @@ var NoticeDataTableSource = {
         'url': '/rest/content/notice/list'
 
         , 'columns': [
-            {'title': '공지 번호', 'data' : 'noticeIdx'}
-            , {'title': '공지구분', 'data' : 'slctType', 'name' : 'sortSlct'}
-            , {'title': '공지 제목', 'data' : 'title', 'render': function(data, type, row, meta) {return '<a href="javascript://" onclick="javascript:getNotice_detail('+meta.row+');">' + data + '</a>'}}
-            , {'title': '상단 고정 여부', 'data' : 'topFix', 'name' : 'sortView'}
-            , {'title': '노출설정', 'data' : 'viewOn', 'name' : ''}
-            , {'title': '작성자 이름', 'data' : 'opName'}
-            , {'title': '작성자번호', 'data' : 'opName'}
-            , {'title': '작성일자', 'data' : 'writeDateFormat'}
-            , {'title': '내용', 'data': 'contents', 'visible': false}
+            {'title': '공지 번호', 'data': 'noticeIdx'}
+            , {'title': '공지구분', 'data': 'slctType', 'name': 'sortSlct'}
+            , {
+                'title': '공지 제목', 'data': 'title', 'render': function (data, type, row, meta) {
+                    return '<a href="javascript://" onclick="javascript:getNotice_detail(' + meta.row + ');">' + data + '</a>'
+                }
+            }
+            , {'title': '상단 고정 여부', 'data': 'topFix', 'name': 'sortView'}
+            , {'title': '노출설정', 'data': 'viewOn', 'name': ''}
+            , {'title': '작성자 이름', 'data': 'opName'}
+            , {'title': '작성자번호', 'data': 'opName'}
+            , {'title': '작성일자', 'data': 'writeDateFormat'}
+            //, {'title': '내용', 'data': 'contents', 'visible': false}
         ]
         , 'comments': ' 달빛라디오 사이트 내 공지를 등록/수정/삭제할 수 있습니다.'
     },
+}
