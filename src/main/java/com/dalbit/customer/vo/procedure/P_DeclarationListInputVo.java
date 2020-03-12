b2d8a7f6d5200b0b7389713be7e64f229350c1e6
강다인 <daink@inforex.co.kr>
2020-03-12 10:36:17 +0900

    신고센터 수정

--- src/main/java/com/dalbit/customer/vo/procedure/P_DeclarationListInputVo.java
+++ src/main/java/com/dalbit/customer/vo/procedure/P_DeclarationListInputVo.java
@@ -1,10 +1,11 @@
 package com.dalbit.customer.vo.procedure;
 
+import com.dalbit.common.vo.SearchVo;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class P_DeclarationListInputVo {
+public class P_DeclarationListInputVo extends SearchVo {
     private int pageNo;
 }
