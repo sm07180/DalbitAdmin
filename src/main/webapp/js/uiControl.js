794bc9fa76b48f80aac4d3f708a6fc0167ea6c20
이재호 <leejaeho114@gmail.com>
2020-03-13 17:32:07 +0900

    검색 결과에 따라 선택삭제, 엑셀 버튼 제어

--- src/main/webapp/js/uiControl.js
+++ src/main/webapp/js/uiControl.js
@@ -1,10 +1,26 @@
-/*검색결과 영역이 접혀 있을 시 열기*/
+$('#_searchToggleIcon').on('click', function(){
+    console.log($(this));
+});
+
+$('.btn-borderless').on('click', function(){
+    console.log($(this));
+});
+
+
 var ui = {
-    toggleSearchList : function(){
+    /*검색결과 영역이 접혀 있을 시 열기*/
+    toggleSearchList(){
         var toggleIcon = $('#_searchToggleIcon');
         if (toggleIcon.hasClass('fa-chevron-down')) {
             toggleIcon.click();
         }
+    },
+
+    /*검색결과에 따라 선택삭제 or excel 버튼 컨트롤*/
+    toogleSearchListFooter(searchCnt){
+        console.log(searchCnt);
+        var target = $("#_searchToggleIcon").closest('.widget').find('.widget-footer');
+        0 < searchCnt ? target.show() : target.hide();
     }
 }
 
