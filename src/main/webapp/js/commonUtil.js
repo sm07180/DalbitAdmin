154794502879f727537dce819523ec7b1bfedbdf
이재호 <leejaeho114@gmail.com>
2020-03-13 16:08:24 +0900

    검색박스 안에 dummy input 자동으로 추가.. 검색어에서 엔터 시 submit 방지.

--- src/main/webapp/js/commonUtil.js
+++ src/main/webapp/js/commonUtil.js
@@ -1,3 +1,8 @@
+/* 화면 로딩 시 검색박스 안에 dummy를 넣어서 검색어에 엔터 누를 시 submit을 방지한다. */
+$(function(){
+    $("#searchForm").append('<input type="text" id="dummy" style="display:none;" />');
+});
+
 /*이벤트 바인딩*/
 $(document).on('click', '._openPop', function(){
     windowOpen($(this).data());
