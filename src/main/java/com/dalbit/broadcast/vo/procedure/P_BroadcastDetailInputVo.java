94fe7fd862766b66c1dce79c272fac909e953375
강다인 <daink@inforex.co.kr>
2020-03-20 13:57:18 +0900

    방송방 상세 조회

---
+++ src/main/java/com/dalbit/broadcast/vo/procedure/P_BroadcastDetailInputVo.java
@@ -0,0 +1,13 @@
+package com.dalbit.broadcast.vo.procedure;
+
+import com.dalbit.common.vo.SearchVo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class P_BroadcastDetailInputVo extends SearchVo {
+
+    private String room_no;
+
+}
---
+++ src/main/java/com/dalbit/broadcast/vo/procedure/P_BroadcastDetailOutputVo.java
@@ -0,0 +1,39 @@
+package com.dalbit.broadcast.vo.procedure;
+
+import com.dalbit.common.vo.BaseVo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class P_BroadcastDetailOutputVo extends BaseVo {
+
+    private String backgroundImage;
+    private String welcomeMsg;
+    private String subjectType;
+    private String title;
+    private String dj_mem_no;
+    private String dj_userId;
+    private String dj_nickName;
+    private String dj_memSex;
+    private String opMemoCnt;
+    private String entryType;
+    private String freezeMsg;
+    private String forcedQuit;
+    private String broadcastState;
+    private String micState;
+    private String guestState;
+    private String guestCnt;
+    private String guest_mem_no;
+    private String guest_userId;
+    private String guest_nickName;
+    private String managerCnt;
+    private String forcedLeaveCnt;
+    private String osType;
+    private String startDate;
+    private String endDate;
+    private String airTime;
+    private String lastOpDate;
+    private String lastOpName;
+
+}
