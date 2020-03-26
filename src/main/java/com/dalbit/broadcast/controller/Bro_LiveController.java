a25ef8af17140a4a1c9b7a610d0dcf0dc6565e9a
고병권 <bgko@inforex.co.kr>
2020-03-26 13:38:02 +0900

    실시간 최신생방송

---
+++ src/main/java/com/dalbit/broadcast/controller/Bro_LiveController.java
@@ -0,0 +1,24 @@
+package com.dalbit.broadcast.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/broadcast/live")
+public class Bro_LiveController {
+
+    /**
+     * 생방송 목록
+     * @param model
+     * @return
+     */
+    @GetMapping("list")
+    public String list(Model model) {
+        return "broadcast/live/list";
+    }
+}
