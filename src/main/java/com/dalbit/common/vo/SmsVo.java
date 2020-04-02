75e25e77633b27aa5efab66017432db64b6e79a4
이재호 <leejaeho114@gmail.com>
2020-04-02 10:58:59 +0900

    SMS 발송모듈 수정 제목 없앰

--- src/main/java/com/dalbit/common/vo/SmsVo.java
+++ src/main/java/com/dalbit/common/vo/SmsVo.java
@@ -7,15 +7,14 @@ import lombok.ToString;
 @Getter @Setter @ToString
 public class SmsVo {
 
-    public SmsVo(String subject, String msgBody, String phoneNo){
-        setSubject(subject);
+    public SmsVo(String msgBody, String phoneNo){
         setMsgBody(msgBody);
         setPhoneNo(phoneNo);
     }
 
     private String phoneNo; //문자받을번호
-    private String subject; //제목
     private String msgBody; //메시지
+    private String subject = ""; //제목
 
     //0(단문)으로 하드코딩.
     private String msgType;
