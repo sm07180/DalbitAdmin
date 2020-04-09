b6d45a4d5f28b7d25db55421cdbe0c713748d2ed
이재호 <leejaeho114@gmail.com>
2020-04-09 14:38:35 +0900

    실시간 최신 생방송 > 프로필이미지 컬럼 width 지정

--- src/main/webapp/js/dataTablesSource/broadcast/broadcastDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/broadcast/broadcastDataTableSource.js
@@ -8,7 +8,7 @@ var BroadcastDataTableSource = {
             {'title': '방송제목', 'data': 'title','width' : '200px', 'render': function (data, type, row, meta) {
                     return util.roomNoLink(data, row.room_no);
                 }},
-            {'title': '프로필이미지', 'data': 'dj_profileImage', 'render' : function(data, type, row){
+            {'title': '프로필이미지', 'data': 'dj_profileImage', 'width' : '80px', 'render' : function(data, type, row){
                     return '<img src="'+ common.profileImage(IMAGE_SERVER_URL,data,row.dj_memSex) +'" width="50px" height="50px" ' +
                             'onclick="fullSize_live(this.src)"/>';
                 }},
