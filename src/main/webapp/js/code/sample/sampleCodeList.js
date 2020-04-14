ce6cc16daadcea8883f3fc815ac92c933bbd5e15
강다인 <daink@inforex.co.kr>
2020-04-14 13:28:51 +0900

    log_error_data 검색 조건 코드 수정

--- src/main/webapp/js/code/sample/sampleCodeList.js
+++ src/main/webapp/js/code/sample/sampleCodeList.js
@@ -3,7 +3,7 @@ var search_osType = [
     , new COMMON_CODE('all', '', '전체')
     , new COMMON_CODE('android', '1', '안드로이드')
     , new COMMON_CODE('ios', '2', 'IOS')
-    , new COMMON_CODE('web', '3', 'WEB')
-    , new COMMON_CODE('api', '4', 'API')
-    , new COMMON_CODE('server', '5', 'SERVER')
+    , new COMMON_CODE('web', 'WEB', 'WEB')
+    , new COMMON_CODE('api', 'API', 'API')
+    , new COMMON_CODE('server', 'server', 'SERVER')
 ];
