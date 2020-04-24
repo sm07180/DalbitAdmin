54f31036ed669e543060473c525d2c25388845b1
강다인 <daink@inforex.co.kr>
2020-04-24 17:55:00 +0900

    금칙어관리 엑셀 출력

--- src/main/java/com/dalbit/content/service/SitebanService.java
+++ src/main/java/com/dalbit/content/service/SitebanService.java
@@ -2,17 +2,20 @@ package com.dalbit.content.service;
 
 import com.dalbit.content.dao.SitebanDao;
 import com.dalbit.content.vo.SitebanVo;
+import com.dalbit.excel.service.ExcelService;
+import com.dalbit.excel.vo.ExcelVo;
 import com.dalbit.util.DalbitUtil;
 import com.dalbit.util.JwtUtil;
 import com.dalbit.util.SocketUtil;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -23,6 +26,9 @@ public class SitebanService {
 
     @Autowired
     SocketUtil socketUtil;
+
+    @Autowired
+    ExcelService excelService;
 
     @Autowired
     JwtUtil jwtUtil;
@@ -53,5 +59,36 @@ public class SitebanService {
         Map<String, Object> data =  socketUtil.setSocket(param,"reqBanWord", gson.toJson(message), jwtUtil.generateToken(DalbitUtil.getProperty("temp.memNo"), true));
 
         return result;
+    }
+
+    /**
+     * 엑셀
+     */
+    public Model getListExcel(Model model) {
+        String banWord = sitebanDao.excelBanword();
+
+        //List<String> list = new ArrayList<String>();
+        String[] splitStr = banWord.split("\\|");
+
+        String[] headers = {"No", "ban_word"};
+        int[] headerWidths = {3000, 9000};
+
+        List<Object[]> bodies = new ArrayList<>();
+        for(int i=0; i<splitStr.length; i++) {
+            HashMap hm = new LinkedHashMap();
+            hm.put("No", i+1);
+            hm.put("ban_word", splitStr[i]);
+
+            bodies.add(hm.values().toArray());
+        }
+
+        ExcelVo vo = new ExcelVo(headers, headerWidths, bodies);
+        SXSSFWorkbook workbook = excelService.excelDownload("금지어 관리",vo);
+        model.addAttribute("locale", Locale.KOREA);
+        model.addAttribute("workbook", workbook);
+        model.addAttribute("workbookName", "금지어 관리");
+
+        return model;
+
     }
 }
