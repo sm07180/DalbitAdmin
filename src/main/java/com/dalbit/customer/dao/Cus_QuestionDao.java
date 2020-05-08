159c64753b77f3fb07c0c175f9915cdc23b64fca
이재호 <leejaeho114@gmail.com>
2020-05-08 16:04:45 +0900

    @Transactional(readOnly = true) 추가

--- src/main/java/com/dalbit/customer/dao/Cus_QuestionDao.java
+++ src/main/java/com/dalbit/customer/dao/Cus_QuestionDao.java
@@ -7,6 +7,7 @@ import com.dalbit.customer.vo.procedure.P_QuestionDetailOutputVo;
 import com.dalbit.customer.vo.procedure.P_QuestionListInputVo;
 import com.dalbit.customer.vo.procedure.P_QuestionListOutputVo;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,13 +15,23 @@ import java.util.List;
 
 @Repository
 public interface Cus_QuestionDao {
+
+    @Transactional(readOnly = true)
     ArrayList<P_QuestionListOutputVo> callQuestionList(ProcedureVo procedureVo);
 
+    @Transactional(readOnly = true)
     P_QuestionDetailOutputVo callServiceCenterQnaDetail(ProcedureVo procedureVo);
+
     ProcedureVo callServiceCenterQnaOperate(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     List<FaqVo> getFaqGroupList();
+
+    @Transactional(readOnly = true)
     List<FaqVo> getFaqSubList(FaqVo faqVo);
 
+    @Transactional(readOnly = true)
     HashMap<P_QuestionListOutputVo,String> callQuestionCountTarget(P_QuestionListInputVo pQuestionListInputVo);
+
     int callQuestionDelete(P_QuestionDeleteVo pQuestionDeleteVo);
 }
--- src/main/java/com/dalbit/enter/dao/Ent_JoinDao.java
+++ src/main/java/com/dalbit/enter/dao/Ent_JoinDao.java
@@ -3,17 +3,31 @@ package com.dalbit.enter.dao;
 import com.dalbit.common.vo.ProcedureVo;
 import com.dalbit.enter.vo.procedure.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface Ent_JoinDao {
 
+    @Transactional(readOnly = true)
     public List<P_JoinTotalOutDetailVo> callJoinTotal(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinGenderOutDetailVo> callJoinGender(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinPlatformGenderOutDetailVo> callJoinPlatformGender(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinPlatformAgeOutDetailVo> callJoinPlatformAge(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinWithdrawOutDetailVo> callJoinWithdraw(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinWithdrawOutDetailVo> callStatJoin(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
     public List<P_JoinWithdrawOutDetailVo> callStatWithdraw(ProcedureVo procedureVo);
 }
