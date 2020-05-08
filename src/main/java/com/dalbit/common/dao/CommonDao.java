2d8cd93ca0525b10a01512763ab93cf6ad98a4b6
이재호 <leejaeho114@gmail.com>
2020-05-08 15:57:13 +0900

    @Transactional(readOnly = true)

--- src/main/java/com/dalbit/common/dao/CommonDao.java
+++ src/main/java/com/dalbit/common/dao/CommonDao.java
@@ -3,13 +3,17 @@ package com.dalbit.common.dao;
 import com.dalbit.common.vo.MenuVo;
 import com.dalbit.common.vo.ProcedureVo;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface CommonDao {
+
+    @Transactional(readOnly = true)
     List<Map> callCodeDefineSelect();
 
+    @Transactional(readOnly = true)
     List<MenuVo> getMenuInfo(MenuVo menuVo);
 }
