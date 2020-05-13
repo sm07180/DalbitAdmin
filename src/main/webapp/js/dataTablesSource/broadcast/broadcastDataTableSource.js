d11a1894b3b4fa700462b236690c6731de9f4221
이재호 <leejaeho114@gmail.com>
2020-05-13 09:19:45 +0900

    폰트색상 변경 스크립트 추가

--- src/main/webapp/js/dataTablesSource/broadcast/broadcastDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/broadcast/broadcastDataTableSource.js
@@ -37,7 +37,7 @@ var BroadcastDataTableSource = {
                     return common.timeStamp(data);
                 }},
             {'title': '나가기<br/>횟수', 'data': 'exit_try_count','width' : '70px','render': function (data){
-                    return common.redFont(data) +'번';
+                    return common.fontColor(data, 1, 'red') +'번';
                 }},
             {'title': '누적<br/>청취자', 'data': 'totalListener','width' : '65px','render': function (data){
                     var tmp = common.addComma(data);
@@ -112,7 +112,7 @@ var BroadcastDataTableSource = {
                     return tmp + "건";
                 }},
             {'title': '나가기<br/>횟수', 'data': 'exit_try_count','width' : '70px','render': function (data){
-                    return common.redFont(data)+'번';;
+                    return common.fontColor(data, 1, 'red')+'번';
                 }},
         ]
         , 'comments': 'ㆍ방송제목을 클릭하시면 현재 방송중인 정보를 확인 할 수 있습니다.'
