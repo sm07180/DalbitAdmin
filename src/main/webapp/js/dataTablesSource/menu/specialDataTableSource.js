743f2a6d1492d54a176885b42a419a00623e9319
강다인 <daink@inforex.co.kr>
2020-05-15 15:41:01 +0900

    스페셜 DJ 개발 중

--- src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
@@ -3,15 +3,15 @@ var specialDataTableSource = {
         'url': '/rest/menu/special/reqDalList'
         , 'columns': [
             {'title': '회원번호', 'data': 'mem_no', 'render': function(data, type, row) {
+                return '<a href="javascript://" class="_openMemberPop" data-memno="' + row.mem_no + '">' + data + '</a>'
+                }}
+            , {'title': '제목', 'data': 'title', 'render': function(data, type, row) {
                 return '<a href="javascript://" class="_reqDalDetail" data-idx="' + row.idx + '">' + data + '</a>'
                 }}
-            , {'title': '제목', 'data': 'title'}
             , {'title': '등록일', 'data': 'reg_date'}
             , {'title': '내용', 'data': 'contents'}
-            , {'title': '상태', 'data': 'state', 'render': function(data, type, row) {
-                if(row.state == 1) {
-                    return '대기';
-                }
+            , {'title': '상태', 'data': 'state', 'render': function(data) {
+                return util.getCommonCodeLabel(data, special_state);
                 }}
             , {'title': '승인자', 'data': 'op_name'}
             , {'title': '승인 일시', 'data': 'last_upd_date'}
@@ -29,7 +29,6 @@ var specialDataTableSource = {
             , {'title': '관리자 등록여부', 'data': 'is_force'}
             , {'title': '순서', 'data': 'order'}
             , {'title': '등록자', 'data': 'op_name'}
-            , {'title': '승인 일시', 'data': 'last_upd_date'}
         ]
         // , 'comments': ''
     }
