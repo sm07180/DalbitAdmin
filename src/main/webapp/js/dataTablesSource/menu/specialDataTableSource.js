037cc91b1a6375e476750d2f7b5797611753ab34
강다인 <daink@inforex.co.kr>
2020-05-20 17:24:59 +0900

    스페셜DJ 개발 중

--- src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
@@ -5,11 +5,13 @@ var specialDataTableSource = {
             {'title': '회원번호', 'data': 'mem_no', 'render': function(data, type, row) {
                 return '<a href="javascript://" class="_openMemberPop" data-memno="' + row.mem_no + '">' + data + '</a>'
                 }}
-            , {'title': '제목', 'data': 'title', 'render': function(data, type, row) {
-                return '<a href="javascript://" class="_reqDalDetail" data-idx="' + row.idx + '">' + data + '</a>'
-                }}
-            , {'title': '등록일', 'data': 'reg_date', 'render': function(data, type, row) {
+            , {'title': '신청일', 'data': 'reg_date', 'render': function(data, type, row) {
                 return common.convertToDate(data, 'YYYY-MM-DD HH:mm:ss');
+                }}
+            , {'title': '이름', 'data': 'mem_name'}
+            , {'title': '연락처', 'data': 'mem_phone'}
+            , {'title': '제목', 'data': 'title', 'render': function(data, type, row) {
+                    return '<a href="javascript://" class="_reqDalDetail" data-idx="' + row.idx + '">' + data + '</a>'
                 }}
             , {'title': '내용', 'data': 'contents'}
             , {'title': '상태', 'data': 'state', 'render': function(data) {
