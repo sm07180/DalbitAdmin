dc72392ca02c4b289707d6c6ac82763d094c4570
강다인 <daink@inforex.co.kr>
2020-05-25 13:22:13 +0900

    테이블 width 수정

--- src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
+++ src/main/webapp/js/dataTablesSource/menu/specialDataTableSource.js
@@ -2,30 +2,26 @@ var specialDataTableSource = {
     'reqSpecialList': {
         'url': '/rest/menu/special/reqDalList'
         , 'columns': [
-            {'title': '회원번호', 'data': 'mem_no', 'render': function(data, type, row) {
-                return '<a href="javascript://" class="_openMemberPop" data-memno="' + row.mem_no + '">' + data + '</a>'
+            {'title': '회원번호', 'data': 'mem_no', 'width' : '10%', 'render': function(data, type, row) {
+                    return '<a href="javascript://" class="_openMemberPop" data-memno="' + row.mem_no + '">' + data + '</a>'
                 }}
-            , {'title': '신청일', 'data': 'reg_date', 'render': function(data, type, row) {
-                return '<div style="width:70px;">' + common.convertToDate(data, 'YYYY-MM-DD HH:mm:ss'); + '</div>'
+            , {'title': '신청일', 'data': 'reg_date', 'width': '10%', 'render': function(data, type, row) {
+                    return common.convertToDate(data, 'YYYY-MM-DD HH:mm:ss');
                 }}
-            , {'title': '이름', 'data': 'mem_name', 'render': function(data) {
-                return '<div style="width:40px;">' + data + '</div>'
+            , {'title': '이름', 'width': '10%', 'data': 'mem_name'}
+            , {'title': '연락처', 'data': 'mem_phone', 'width':'10%', 'render': function(data){
+                    return common.phoneNumHyphen(data)
                 }}
-            , {'title': '연락처', 'data': 'mem_phone', 'render': function(data){
-                return '<div style="width:60px;">' + common.phoneNumHyphen(data) + '</div>'
-                }}
-            , {'title': '제목', 'data': 'title', 'render': function(data, type, row) {
+            , {'title': '제목', 'data': 'title', 'width': '10%', 'render': function(data, type, row) {
                     return '<a href="javascript://" class="_reqDalDetail" data-idx="' + row.idx + '">' + data + '</a>'
                 }}
-            , {'title': '내용', 'data': 'contents'}
-            , {'title': '상태', 'data': 'state', 'render': function(data) {
-                return '<div style="width:40px;">' + util.getCommonCodeLabel(data, special_state); + '</div>'
+            , {'title': '내용', 'data': 'contents', 'width' : '20%'}
+            , {'title': '상태', 'data': 'state',  'width': '10%','render': function(data) {
+                    return util.getCommonCodeLabel(data, special_state);
                 }}
-            , {'title': '처리자', 'data': 'op_name', 'render': function(data) {
-                    return '<div style="width:40px;">' + data + '</div>'
-                }}
-            , {'title': '처리 일시', 'data': 'last_upd_date', 'render': function(data, type, row) {
-                    return '<div style="width:70px;">' + common.convertToDate(data, 'YYYY-MM-DD HH:mm:ss'); + '</div>'
+            , {'title': '처리자', 'width': '10%', 'data': 'op_name'}
+            , {'title': '처리 일시', 'data': 'last_upd_date', 'width': '10%', 'render': function(data, type, row) {
+                    return common.convertToDate(data, 'YYYY-MM-DD HH:mm:ss');
                 }}
         ]
         // , 'comments': ''
