056de4f0a6145a11dce8f4c7c784d8fdd6f5deff
이재호 <leejaeho114@gmail.com>
2020-06-03 09:11:30 +0900

    수정.

--- src/main/java/com/dalbit/common/view/MessageView.java
+++ src/main/java/com/dalbit/common/view/MessageView.java
@@ -1,10 +1,6 @@
 package com.dalbit.common.view;
 
-import com.dalbit.common.code.Status;
-import com.dalbit.common.vo.JsonOutputVo;
-import com.dalbit.util.GsonUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.view.AbstractView;
 
 import javax.servlet.http.HttpServletRequest;
