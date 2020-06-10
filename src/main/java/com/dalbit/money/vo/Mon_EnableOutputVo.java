6eb64df747d200b0e0d9eba4b6c445a37c1ae7e3
고병권 <bgko@inforex.co.kr>
2020-06-10 16:33:55 +0900

    수정

--- src/main/java/com/dalbit/money/vo/Mon_EnableOutputVo.java
+++ src/main/java/com/dalbit/money/vo/Mon_EnableOutputVo.java
@@ -11,6 +11,7 @@ public class Mon_EnableOutputVo {
     private String mem_no;
     private String mem_userid;
     private String mem_nick;
+    private String image_profile;
     private int mem_state;
     private String mem_sex;
     private int gold;
