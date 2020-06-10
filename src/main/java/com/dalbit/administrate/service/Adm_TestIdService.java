82a8fefae90f6104c41fe909b891ab4ce38dc85a
고병권 <bgko@inforex.co.kr>
2020-06-10 14:33:31 +0900

    null 처리 추가

--- src/main/java/com/dalbit/administrate/service/Adm_TestIdService.java
+++ src/main/java/com/dalbit/administrate/service/Adm_TestIdService.java
@@ -157,7 +157,7 @@ public class Adm_TestIdService {
             hm.put("mem6", DalbitUtil.isEmpty(list.get(i).getMem_nick()) ? "" : list.get(i).getMem_nick());
             hm.put("mem7", mem7);
             hm.put("mem8", list.get(i).getType() + ": " + list.get(i).getCharge() +" / "+ list.get(i).getChargeDate());
-            hm.put("mem9", "달: " + list.get(i).getDal() + " / 별: " + list.get(i).getByeol());
+            hm.put("mem9", DalbitUtil.isEmpty(list.get(i).getDal()) ? "" : "달: " + list.get(i).getDal() + " / 별: " + list.get(i).getByeol());
             hm.put("mem10", DalbitUtil.isEmpty(list.get(i).getMem_phone()) ? "" : list.get(i).getMem_phone());
             hm.put("mem11",  list.get(i).getLevel() + " / " + list.get(i).getGrade());
             hm.put("mem12", DalbitUtil.isEmpty(list.get(i).getReg_date()) ? "" : list.get(i).getReg_date());
