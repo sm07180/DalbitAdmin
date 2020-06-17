31f50abfee466773d3e9645180142d421a5c54e7
이형원 <hwlee@inforex.co.kr>
2020-06-17 13:00:30 +0900

    만나이 계산 추가

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -383,3 +383,22 @@ common.koreaAge = function(birthDate){
         return koreaAge;
     }
 }
+
+common.calcAge = function(birth) {
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1);
+    var day = date.getDate();
+    if (month < 10) month = '0' + month;
+    if (day < 10) day = '0' + day;
+    var monthDay = month + day;
+
+    birth = birth.replace('-', '');
+    var birthdayy = birth.substr(0, 4);
+    var birthdaymd = birth.substr(4, 4);
+    var age = monthDay < birthdaymd ? year - birthdayy - 1 : year - birthdayy;
+
+    return age;
+
+}
