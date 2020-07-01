656e648bef7d822329505fe7e177c0fd26f25a82
고병권 <bgko@inforex.co.kr>
2020-07-01 14:15:03 +0900

    코드 목록 조회 공통 개발

--- src/main/java/com/dalbit/common/controller/CommonController.java
+++ src/main/java/com/dalbit/common/controller/CommonController.java
@@ -1,17 +1,25 @@
 package com.dalbit.common.controller;
 
 import com.dalbit.common.annotation.NoLogging;
+import com.dalbit.common.code.Status;
 import com.dalbit.common.service.CommonService;
+import com.dalbit.common.vo.CodeListVo;
+import com.dalbit.common.vo.CodeVo;
+import com.dalbit.common.vo.JsonOutputVo;
 import com.dalbit.util.GsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("common")
 public class CommonController {
 
     @Autowired
@@ -24,6 +32,15 @@ public class CommonController {
     @GetMapping("/ctrl/check/service")
     public String checkService(HttpServletRequest request){
         return commonService.checkHealthy(request);
+    }
+
+    /**
+     * 코드목록
+     */
+    @PostMapping("codeList")
+    public String codeList(CodeListVo codeListVo){
+        String codeList = commonService.getCodeList(codeListVo);
+        return codeList;
     }
 
 }
