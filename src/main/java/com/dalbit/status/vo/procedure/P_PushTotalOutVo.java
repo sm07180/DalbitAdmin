14aff75691c74115f63d1cc12a1feea040ad0dc3
전유신 <yoosin0513@inforex.co.kr>
2020-07-13 13:24:24 +0900

    1. '서비스 현황 > PUSH 현황'  성공율 로직 수정

--- src/main/java/com/dalbit/status/vo/procedure/P_PushTotalOutVo.java
+++ src/main/java/com/dalbit/status/vo/procedure/P_PushTotalOutVo.java
@@ -10,17 +10,21 @@ import lombok.ToString;
 @ToString
 public class P_PushTotalOutVo{
 
-    private int sum_male_send_cnt;
-    private int sum_male_succ_cnt;
-    private int sum_male_fail_cnt;
-    private int sum_female_send_cnt;
-    private int sum_female_succ_cnt;
-    private int sum_female_fail_cnt;
-    private int sum_none_send_cnt;
-    private int sum_none_succ_cnt;
-    private int sum_none_fail_cnt;
-    private int sum_total_send_cnt;
-    private int sum_total_succ_cnt;
-    private int sum_total_fail_cnt;
+    private String sum_male_send_cnt;
+    private String sum_male_succ_cnt;
+    private String sum_male_fail_cnt;
+    private String sum_male_succ_rate;
+    private String sum_female_send_cnt;
+    private String sum_female_succ_cnt;
+    private String sum_female_fail_cnt;
+    private String sum_female_succ_rate;
+    private String sum_none_send_cnt;
+    private String sum_none_succ_cnt;
+    private String sum_none_fail_cnt;
+    private String sum_none_succ_rate;
+    private String sum_total_send_cnt;
+    private String sum_total_succ_cnt;
+    private String sum_total_fail_cnt;
+    private String sum_total_succ_rate;
 
 }
