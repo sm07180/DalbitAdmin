de299893818aff30d04866fd77cf107e7e7a7317
이형원 <hwlee@inforex.co.kr>
2020-07-22 17:26:04 +0900

    쿠콘 현금영수증 발행내역 조회 추가 진행중

--- src/main/java/com/dalbit/payment/vo/Pay_CooconReceiptOutputVo.java
+++ src/main/java/com/dalbit/payment/vo/Pay_CooconReceiptOutputVo.java
@@ -1,18 +1,18 @@
 package com.dalbit.payment.vo;
 
+import com.dalbit.common.vo.PagingVo;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Pay_CooconReceiptOutputVo {
-
-    private int rowNum;
+public class Pay_CooconReceiptOutputVo extends PagingVo {
 
     private String pay_ok_date;
     private String rcpt_nm;
     private String order_id;
     private String mem_name;
-    private String mem_state;
-    private String image_profile;
+    private String receipt_ok_number;
+    private String receipt_code;
+    private int pay_amt;
 }
