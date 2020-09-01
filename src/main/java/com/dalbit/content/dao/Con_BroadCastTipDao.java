8cb40ffc91455db7135459c3d184df09751a503e
이형원 <hwlee@inforex.co.kr>
2020-09-01 17:11:55 +0900

    방송팁 관리

---
+++ src/main/java/com/dalbit/content/dao/Con_BroadCastTipDao.java
@@ -0,0 +1,30 @@
+package com.dalbit.content.dao;
+
+import com.dalbit.common.vo.ProcedureVo;
+import com.dalbit.content.vo.BoardAdmFanBoardDeleteVo;
+import com.dalbit.content.vo.BoardAdmFanBoardReplyVo;
+import com.dalbit.content.vo.BoardAdmFanBoardVo;
+import com.dalbit.content.vo.BoardAdmStoryVo;
+import com.dalbit.content.vo.procedure.P_BroadCastTipOutputVo;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Repository
+public interface Con_BroadCastTipDao {
+
+    @Transactional(readOnly = true)
+    ArrayList<P_BroadCastTipOutputVo> callBroadcastTipList(ProcedureVo procedureVo);
+
+    @Transactional(readOnly = true)
+    P_BroadCastTipOutputVo callBroadcastTipDetail(ProcedureVo procedureVo);
+
+    ProcedureVo callbroadCastTipEdit(ProcedureVo procedureVo);
+
+    ProcedureVo callbroadCastTipAdd(ProcedureVo procedureVo);
+
+    ProcedureVo callbroadCastTipDel(ProcedureVo procedureVo);
+
+
+}
---
+++ src/main/java/com/dalbit/content/vo/procedure/P_BroadCastTipOutputVo.java
@@ -0,0 +1,18 @@
+package com.dalbit.content.vo.procedure;
+
+import com.dalbit.common.vo.BaseVo;
+import com.dalbit.util.DalbitUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class P_BroadCastTipOutputVo extends BaseVo {
+    private int idx;
+    private String viewTarget;
+    private String tipDesc;
+    private int viewOn;
+    private Date updateDate;
+}
