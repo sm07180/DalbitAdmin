8ff20dfdcf7f017796747c12e566b3e979ee5b99
이재호 <leejaeho114@gmail.com>
2020-09-02 15:47:33 +0900

    장비지원이벤트 추가

--- src/main/java/com/dalbit/common/code/EventCode.java
+++ src/main/java/com/dalbit/common/code/EventCode.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public enum EventCode {
     인증샷(2, "2020.08 PC인증샷 이벤트"),
+    노하우(3, "2020.08 방송 노하우 이벤트"),
+    방송장비(4, "2020.09 방송장비 이벤트"),
 
     ;
     final private int eventIdx;
