f4ba8fc0bd3740f44501d5ac57b248efda251f6c
고병권 <bgko@inforex.co.kr>
2020-09-07 16:02:25 +0900

    방송현황 리뉴얼

---
+++ src/main/java/com/dalbit/status/vo/procedure/P_NewBroadcastGiftOutputVo.java
@@ -0,0 +1,34 @@
+package com.dalbit.status.vo.procedure;
+
+import com.dalbit.common.vo.BaseVo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class P_NewBroadcastGiftOutputVo extends BaseVo {
+
+    private String purchaseDate;
+    private String mem_no;
+    private String mem_userid;
+    private String mem_nick;
+    private String mem_sex;
+    private String mem_birth_year;
+    private String mem_birth_month;
+    private String mem_birth_day;
+    private String item_code;
+    private String item_name;
+    private String item_thumbnail;
+    private int item_type;
+    private String room_no;
+    private String title;
+    private int itemCnt;
+    private int itemAmt;
+    private String gifted_mem_no;
+    private String gifted_mem_userid;
+    private String gifted_mem_nick;
+    private String webp_image;
+
+    private int totalCnt;
+
+}
