250e0c701391c16bdfa455cf37a885d7e06a0d5c
고병권 <bgko@inforex.co.kr>
2020-09-10 11:34:18 +0900

    회원 상세정보 통계 추가 신고 구분 '닉네임' 추가, 프로필사진->이미지 변경

---
+++ src/main/java/com/dalbit/member/vo/procedure/P_MemberAccumOutputVo.java
@@ -0,0 +1,39 @@
+package com.dalbit.member.vo.procedure;
+
+import com.dalbit.common.vo.SearchVo;
+import com.dalbit.util.DalbitUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+public class P_MemberAccumOutputVo{
+
+    private int payCnt;
+    private int payAmt;
+    private int payCanCnt;
+    private int payCanAmt;
+    private int brodCnt;
+    private int broadTime;
+    private int byeolCnt;
+    private int listenerCnt;
+    private int goodCnt;
+    private int listenCnt;
+    private int listenTime;
+    private int dalCnt;
+    private int sendDal;
+    private int giftedDal;
+    private int exchangeCnt;
+    private int exchangeAmt;
+    private int exchangeCancelCnt;
+    private int exchangeCancelAmt;
+    private int changeCnt;
+    private int changeByeol;
+    private int clipCnt;
+    private int clipByeol;
+    private int clipPlay;
+    private int clipGood;
+
+}
