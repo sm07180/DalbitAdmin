8c884e9ec41eebdf946a12e1c17d4b2afb9d772d
이재은 <coreano81@inforex.co.kr>
2020-09-16 17:30:08 +0900

    샘플 슬래이브db 적용

--- src/main/java/com/dalbit/sample/service/SampleService.java
+++ src/main/java/com/dalbit/sample/service/SampleService.java
@@ -14,14 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
 import java.util.*;
 
 @Slf4j
 @Service
-@Transactional
 public class SampleService {
 
     @Autowired
