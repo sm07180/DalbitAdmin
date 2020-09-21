28c932fb87b8a860cf9071f30c02bea6ae47d61b
이재호 <leejaeho114@gmail.com>
2020-09-21 16:17:39 +0900

    에러 로그 VO 생성

---
+++ src/main/java/com/dalbit/common/vo/procedure/P_ErrorLogVo.java
@@ -0,0 +1,29 @@
+package com.dalbit.common.vo.procedure;
+
+import com.dalbit.common.vo.request.ErrorLogVo;
+import com.dalbit.member.vo.MemberVo;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Getter @Setter
+public class P_ErrorLogVo {
+
+    private String mem_no;
+    private String os;
+    private String dtype;
+    private String ctype;
+    private String desc;
+    private String version;
+    private String build;
+    public P_ErrorLogVo(){}
+    public P_ErrorLogVo(ErrorLogVo errorLogVo, HttpServletRequest request){
+        setMem_no(MemberVo.getMyMemNo());
+        setOs(errorLogVo.getOs());
+        setDtype(errorLogVo.getDataType());
+        setCtype(errorLogVo.getCommandType());
+        setDesc(errorLogVo.getDesc());
+    }
+
+}
