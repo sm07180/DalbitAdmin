f250a7f3539c5d519ff45edbbecf64e74e151ded
고병권 <bgko@inforex.co.kr>
2020-10-14 09:18:47 +0900

    myStar/myFan 쿼리 튜닝

--- src/main/java/com/dalbit/member/service/Mem_MystarService.java
+++ src/main/java/com/dalbit/member/service/Mem_MystarService.java
@@ -35,15 +35,15 @@ public class Mem_MystarService {
         ArrayList<P_MemberMystarOutputVo> mystarList = mem_MystarDao.callMystarHistory(pMemberMystarInputVo);
         int totalCnt = mem_MystarDao.callMystarHistory_totalCnt(pMemberMystarInputVo);
 
-        for(int i=0;i<mystarList.size();i++){
-            MemberVo outVo = mem_MemberDao.getMemberInfo(mystarList.get(i).getMem_no_star());
-            if(!DalbitUtil.isEmpty(outVo)) {
-                mystarList.get(i).setStar_sex(outVo.getMem_sex());
-                mystarList.get(i).setMem_birth_year(outVo.getMem_birth_year());
-                mystarList.get(i).setMem_birth_month(outVo.getMem_birth_month());
-                mystarList.get(i).setMem_birth_day(outVo.getMem_birth_day());
-            }
-        }
+//        for(int i=0;i<mystarList.size();i++){
+//            MemberVo outVo = mem_MemberDao.getMemberInfo(mystarList.get(i).getMem_no_star());
+//            if(!DalbitUtil.isEmpty(outVo)) {
+//                mystarList.get(i).setStar_sex(outVo.getMem_sex());
+//                mystarList.get(i).setMem_birth_year(outVo.getMem_birth_year());
+//                mystarList.get(i).setMem_birth_month(outVo.getMem_birth_month());
+//                mystarList.get(i).setMem_birth_day(outVo.getMem_birth_day());
+//            }
+//        }
         int liveTotalCnt = mem_MystarDao.callMystar_totalCnt(pMemberMystarInputVo);
         String result;
         result = gsonUtil.toJson(new JsonOutputVo(Status.MyStar목록보기성공, mystarList, new PagingVo(totalCnt), liveTotalCnt));
@@ -60,15 +60,15 @@ public class Mem_MystarService {
         ArrayList<P_MemberMystarOutputVo> myfanList = mem_MystarDao.callMyfanHistory(pMemberMystarInputVo);
         int totalCnt = mem_MystarDao.callMyfanHistory_totalCnt(pMemberMystarInputVo);
 
-        for(int i=0;i<myfanList.size();i++){
-            MemberVo outVo = mem_MemberDao.getMemberInfo(myfanList.get(i).getMem_no_fan());
-            if(!DalbitUtil.isEmpty(outVo)) {
-                myfanList.get(i).setFan_sex(outVo.getMem_sex());
-                myfanList.get(i).setMem_birth_year(outVo.getMem_birth_year());
-                myfanList.get(i).setMem_birth_month(outVo.getMem_birth_month());
-                myfanList.get(i).setMem_birth_day(outVo.getMem_birth_day());
-            }
-        }
+//        for(int i=0;i<myfanList.size();i++){
+//            MemberVo outVo = mem_MemberDao.getMemberInfo(myfanList.get(i).getMem_no_fan());
+//            if(!DalbitUtil.isEmpty(outVo)) {
+//                myfanList.get(i).setFan_sex(outVo.getMem_sex());
+//                myfanList.get(i).setMem_birth_year(outVo.getMem_birth_year());
+//                myfanList.get(i).setMem_birth_month(outVo.getMem_birth_month());
+//                myfanList.get(i).setMem_birth_day(outVo.getMem_birth_day());
+//            }
+//        }
 
         int liveTotalCnt = mem_MystarDao.callMyfan_totalCnt(pMemberMystarInputVo);
         String result;
