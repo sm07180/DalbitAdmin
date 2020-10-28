3366b5e1e8c61e55edcfdd5eae859001fbcdf712
고병권 <bgko@inforex.co.kr>
2020-10-28 11:00:28 +0900

    방송현황 UI, 데이터 수정

--- src/main/java/com/dalbit/status/vo/procedure/P_BroadcastGiftOutVo.java
+++ src/main/java/com/dalbit/status/vo/procedure/P_BroadcastGiftOutVo.java
@@ -9,10 +9,19 @@ import lombok.Setter;
 public class P_BroadcastGiftOutVo extends BaseVo {
 
     private int sum_totalGiftCnt;
+    private int sum_totalGiftCmt;
     private int sum_totalGiftAmount;
     private int sum_normalGiftCnt;
+    private int sum_normalGiftCmt;
     private int sum_normalGiftAmount;
     private int sum_secretGiftCnt;
+    private int sum_secretGiftCmt;
     private int sum_secretGiftAmount;
+    private int sum_guest_normalGiftCnt;
+    private int sum_guest_normalGiftCmt;
+    private int sum_guest_normalGiftAmount;
+    private int sum_guest_secretGiftCnt;
+    private int sum_guest_secretGiftCmt;
+    private int sum_guest_secretGiftAmount;
 
 }
