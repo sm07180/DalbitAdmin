0cb8f683db63e25af6e9914a2898cfe043fd2ba9
pogus55 <pogus55@yeoboyacorp.com>
2022-04-08 15:36:23 +0900

    # 팀제도 내용수정

--- src/main/java/com/dalbit/content/dao/Con_TeamDao.java
+++ src/main/java/com/dalbit/content/dao/Con_TeamDao.java
@@ -1,6 +1,6 @@
 package com.dalbit.content.dao;
 
-import com.dalbit.content.vo.procedure.P_TeamSearchInputVo;
+import com.dalbit.content.vo.procedure.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -41,4 +41,46 @@ public interface Con_TeamDao {
      * @return
      */
     List<Object> selectTeamDeletedMember(P_TeamSearchInputVo pTeamSearchInputVo);
+
+    /**
+     * 팀 심볼 등록
+     * @param pTeamSymbolInputVo
+     * @return
+     */
+    int insertTeamSymbol(P_TeamSymbolInputVo pTeamSymbolInputVo);
+
+    /**
+     * 팀 심볼 수정
+     * @param pTeamSymbolInputVo
+     * @return
+     */
+    int updateTeamSymbol(P_TeamSymbolInputVo pTeamSymbolInputVo);
+
+    /**
+     * 팀 활동배지 수정
+     * @param pTeamBadgeInputVo
+     * @return
+     */
+    int updateTeamBadge(P_TeamBadgeInputVo pTeamBadgeInputVo);
+
+    /**
+     * 팀 심볼 리스트
+     * @param pTeamSearchSymbolInputVo
+     * @return
+     */
+    List<Object> selectTeamsSymbols(P_TeamSearchSymbolInputVo pTeamSearchSymbolInputVo);
+
+    /**
+     * 팀 활동배지 리스트
+     * @param pTeamSearchSymbolInputVo
+     * @return
+     */
+    List<Object> selectTeamsBadges(P_TeamSearchSymbolInputVo pTeamSearchSymbolInputVo);
+
+    /**
+     * 팀 집계
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    P_TeamMonthStatOutputVo selectTeamsMonthStat(P_TeamSearchInputVo pTeamSearchInputVo);
 }
