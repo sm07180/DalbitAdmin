572602143b520ecd6b327b01052845f87d19ef12
pogus55 <pogus55@yeoboyacorp.com>
2022-04-08 13:28:33 +0900

    # 팀제도 Dao 추가

--- src/main/java/com/dalbit/content/dao/Con_TeamDao.java
+++ src/main/java/com/dalbit/content/dao/Con_TeamDao.java
@@ -1,8 +1,44 @@
 package com.dalbit.content.dao;
 
+import com.dalbit.content.vo.procedure.P_TeamSearchInputVo;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface Con_TeamDao {
+    /**
+     * 팀 목록
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    List<Object> selectTeams(P_TeamSearchInputVo pTeamSearchInputVo);
 
+    /**
+     * 팀 목록 (삭제)
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    List<Object> selectTeamsDeleted(P_TeamSearchInputVo pTeamSearchInputVo);
+
+    /**
+     * 팀 상세
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    List<Object> selectTeam(P_TeamSearchInputVo pTeamSearchInputVo);
+
+    /**
+     * 팀 상세 (삭제)
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    List<Object> selectTeamDeleted(P_TeamSearchInputVo pTeamSearchInputVo);
+
+    /**
+     * 팀 상세 - 팀원 (삭제)
+     * @param pTeamSearchInputVo
+     * @return
+     */
+    List<Object> selectTeamDeletedMember(P_TeamSearchInputVo pTeamSearchInputVo);
 }
