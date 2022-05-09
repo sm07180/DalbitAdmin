52470a4e65ab9db55dafbececf551542f5b83b65
이정혁 <habetree@gmail.com>
2022-05-09 09:51:57 +0900

    #7882 스케쥴러 분리

--- src/main/java/com/dalbit/schelduler/DallaScheduler.java
+++ src/main/java/com/dalbit/schelduler/DallaScheduler.java
@@ -1,6 +1,6 @@
 package com.dalbit.schelduler;
 
-import com.dalbit.administrate.service.Adm_AlarmTalkService;
+import com.dalbit.schelduler.service.LuckyChatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -9,16 +9,15 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Profile({"dev"})
-public class AlarmTalkScheduler {
+@Profile({"real"})
+public class DallaScheduler {
 
     @Autowired
-    Adm_AlarmTalkService admAlarmTalkService;
+    LuckyChatService luckyChatService;
 
+    // 00시부터 1시간 간격마다 럭키 채팅 관련(키보드워리어) 스케쥴러 실행
     @Scheduled(cron = "0 0 0/1 * * *")
-    protected void sendStatusAlarmTalk() {
-        // 달빛라이브 통계 알림톡 발송 요청
-        admAlarmTalkService.sendStatusAlarmTalk();
+    public void luckyChatRewardChoice() {
+        luckyChatService.luckyChatRewardChoice();
     }
-
 }
