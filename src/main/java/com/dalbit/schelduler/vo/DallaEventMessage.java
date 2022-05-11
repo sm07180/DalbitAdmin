e7e27717958350b6001e499b1d574634286f2e41
이정혁 <habetree@gmail.com>
2022-05-11 09:07:24 +0900

    #7882 럭키채팅 로직 수정

--- src/main/java/com/dalbit/schelduler/vo/DallaEventMessage.java
+++ src/main/java/com/dalbit/schelduler/vo/DallaEventMessage.java
@@ -8,5 +8,6 @@ public class DallaEventMessage {
     private String content = ""; // 이벤트 메시지 내용
     private String linkTitle = ""; // 이벤트 링크 버튼 타이틀
     private String linkUrl = ""; // 이벤트 Path
+    private String nickName = ""; // 이벤트 대상 회원 닉네임
     private String type = "normal"; // 이벤트 메시지 타입
 }
